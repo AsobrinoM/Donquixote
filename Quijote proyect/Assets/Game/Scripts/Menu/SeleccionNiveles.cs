7a35fa54a978bceb4e1620ba161e26fa7deaf201
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2024-03-01 17:20:14 +0100

    Seleccion de niveles + Fundido a Negro cuando seleccionas el nivel

---
+++ Quijote proyect/Assets/Game/Scripts/Menu/SeleccionNiveles.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SeleccionNiveles : MonoBehaviour
+{
+
+    [SerializeField] public GameObject btnNivel1;
+    [SerializeField] public GameObject btnNivel2;
+    [SerializeField] public GameObject btnNivel3;
+    [SerializeField] public GameObject btnNivel4;
+
+    [SerializeField] private Image blackOverlay;
+
+    void Start()
+    {
+        blackOverlay.enabled = false;
+
+        if (PlayerPrefs.GetInt("Nivel1") == 1)
+        {
+            btnNivel1.SetActive(false);
+        }
+        if (PlayerPrefs.GetInt("Nivel2") == 1)
+        {
+            btnNivel2.SetActive(false);
+        }
+        if (PlayerPrefs.GetInt("Nivel3") == 1)
+        {
+            btnNivel3.SetActive(false);
+        }
+        if (PlayerPrefs.GetInt("Nivel4") == 1)
+        {
+            btnNivel4.SetActive(false);
+        }
+    }
+
+    public void Nivel1()
+    {
+        FadeManager.Instance.FadeToScene("Tutorial");
+    }
+
+    public void Nivel2()
+    {
+        FadeManager.Instance.FadeToScene("Escena Antonio");
+    }
+
+    public void Nivel3()
+    {
+        FadeManager.Instance.FadeToScene("Escena Alex");
+    }
+
+    public void Nivel4()
+    {
+        FadeManager.Instance.FadeToScene("Nivel Final");
+    }
+}
