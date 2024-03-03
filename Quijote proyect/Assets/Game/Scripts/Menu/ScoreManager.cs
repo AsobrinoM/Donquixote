08612eb6e6aae4ce090a2c85d238367c9dbb8952
Alseona08 <aserranoo08@educastillalamancha.es>
2024-03-03 19:50:37 +0100

    Añadido el ScoreManager en el Menu

---
+++ Quijote proyect/Assets/Game/Scripts/Menu/ScoreManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+
+    public int HighScore1;
+
+    public int HighScore2;
+
+    public int HighScore3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        HighScore1 = 0;
+        HighScore2 = 0;
+        HighScore3 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(HighScore1);
+    }
+}
