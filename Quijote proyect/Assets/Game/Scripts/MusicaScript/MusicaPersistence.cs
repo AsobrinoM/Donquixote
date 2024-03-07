87083f30824d1a797cb844f6fcc3e8ac588d4911
AsobrinoM <asobrinom16@educastillalamancha.com>
2024-03-07 16:00:36 +0100

    Musica Tutorial

--- Quijote proyect/Assets/Game/Scripts/MusicaScript/MusicaPersistence.cs
+++ Quijote proyect/Assets/Game/Scripts/MusicaScript/MusicaPersistence.cs
@@ -1,25 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicaPersistente : MonoBehaviour
 {
+    // Declara una variable para almacenar la instancia
     private static MusicaPersistente instancia;
+
+    // Declara una variable para el audio source
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip musicaMenu;
+    [SerializeField] private AudioClip musicaTutorial;
+    private AudioClip ultimoClipReproducido;
 
     void Awake()
     {
         if (instancia == null)
         {
-            // Si no hay instancia, establece esta como la instancia
             instancia = this;
-            // Hacer que este objeto persista entre las escenas
             DontDestroyOnLoad(gameObject);
+
+            audioSource = GetComponent<AudioSource>();
         }
         else
         {
-            // Si ya hay una instancia, destruir este objeto
+     
             Destroy(gameObject);
         }
     }
-  
-}
+
+    void Start()
+    {
+        ultimoClipReproducido=audioSource.clip;
+        SceneManager.sceneLoaded += CambiarMusicaSegunEscena;
+    }
+
+    void CambiarMusicaSegunEscena(Scene escena, LoadSceneMode modo)
+    {
+        AudioClip nuevoClip = null;
+        switch (escena.name)
+        {
+            case "Menu":
+                nuevoClip = musicaMenu;
+                break;
+            case "Tutorial":
+                nuevoClip = musicaTutorial;
+                break;
+               
+        }
+        if (nuevoClip != null && nuevoClip != ultimoClipReproducido)
+        {
+            audioSource.clip = nuevoClip;
+            audioSource.Play();
+            ultimoClipReproducido = nuevoClip;
+        }
+       
+    }
+
+       
+    }
